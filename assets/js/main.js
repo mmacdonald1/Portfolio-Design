e897c1db1e995789d5cf86245779c4e0baade706
Melanie MacDonald <melaniemacdonald@Melanies-MBP.fios-router.home>
2017-12-28 18:26:32 -0500

    mobile

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$("#dropdown-menu").on("click", function dropdown(){ 
+    if(document.getElementById("menu-items").style.display = 'none')
+      {
+        document.getElementById("menu-items").style.display = 'block';
+      }
+      else if(document.getElementById("menu-items").style.display = 'block')
+      {
+        document.getElementById("menu-items").style.display = 'none';
+      }
+    
+});
+
