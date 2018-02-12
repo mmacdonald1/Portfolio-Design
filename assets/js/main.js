ef427101a9c62593d0a5ebabbdfe4020baae2eb0
Melanie MacDonald <melmacd94@gmail.com>
2018-02-12 12:38:07 -0500

    mobile toggle start

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,10 @@
-$("#dropdown-menu").on("click", function dropdown(){ 
-    if(document.getElementById("menu-items").style.display = 'none')
-      {
-        document.getElementById("menu-items").style.display = 'block';
-      }
-      else if(document.getElementById("menu-items").style.display = 'block')
-      {
-        document.getElementById("menu-items").style.display = 'none';
-      }
-    
+$(document).ready(function() {
+  $('#dropdown-menu').on('click', function(event) {
+    $('.nav-items').toggleClass('hidden');
+      
+  });
+
+  $('.nav-items').on('click', function(event){
+      $('.nav-items').toggleClass('hidden');
+  });
 });
-
-// Get the modal
-var modal = document.getElementById('Mind_Reader_modal');
-
-console.log(modal);
-
-// Get the button that opens the modal
-var btn = document.getElementById("mind-reader-overlay");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
